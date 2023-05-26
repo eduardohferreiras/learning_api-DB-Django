499c5b1e5ca21e470e70a32280b7a40694ab609c
Eduardo Henrique Ferreira Silva <eduardohenriqueferreirasilva@Eduardos-MacBook-Air.local>
2023-05-25 23:36:43 -0300

    Initial mods to models, first migrations.

--- e3_db/socialnetwork/core/models.py
+++ e3_db/socialnetwork/core/models.py
@@ -1,12 +1,9 @@
 from django.db import models
 
-class Profile():   
-    def __init__(self, email, id, isHidden = False):
-        self.id = id
-        self.email = email
-        self.isHidden = isHidden
+class Profile(models.Model):   
+    email = models.EmailField()
+    isHidden = models.BooleanField (default=False)
     
-class Connection():
-    def __init__(self, id1, id2):
-        self.id1 = id1
-        self.id2 = id2
+class Connection(models.Model):
+    id1 = models.ForeignKey("Profile", on_delete=models.CASCADE, related_name="id1")
+    id2 = models.ForeignKey("Profile", on_delete=models.CASCADE, related_name="id2")
