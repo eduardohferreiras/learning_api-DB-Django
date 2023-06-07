1d9b68aa0ec7cc305ca114b7a4b0326199220293
Eduardo Henrique Ferreira Silva <eduardohenriqueferreirasilva@Eduardos-MacBook-Air.local>
2023-06-06 22:23:09 -0300

    Removing exercise #4 temporarily.

--- e4_docker/README.md
+++
@@ -1,5 +0,0 @@
-# learning_api-DB-Django - Exercise 4
-
-## Instructions
-
-Configure production-like container for this application, using Docker. Write instruction for running this aplication in a new machine in the e4_instruction.md file.
--- e4_docker/socialnetwork/core/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-class Profile(models.Model):   
-    email = models.EmailField()
-    isHidden = models.BooleanField(default=False)
-    
-class Connection(models.Model):
-    id1 = models.ForeignKey("Profile", on_delete=models.CASCADE, related_name="id1")
-    id2 = models.ForeignKey("Profile", on_delete=models.CASCADE, related_name="id2")
